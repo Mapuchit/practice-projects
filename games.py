56c7ed0a60aadcbb3b42def940d1c44d77c5fb84
unknown <59705658+Mapuchit@users.noreply.github.com>
2020-07-27 18:45:20 +0100

    cho_han game function finished and tested

--- games.py
+++ games.py
@@ -4,29 +4,32 @@ money = 100
 
 ## Game of chance functions:
 
+
 # Coin flip
 
 def coinflip():
   # will increment money in global wallet
   global money
-  # get input for bet and choice
+  # get input for bet (repeats)
   print("Welcome to Coin Flip!")
   print("You have £{0} in your wallet.".format(money))
   bet = int(input("How much money would you like to bet? Please enter a whole number: "))
   if bet > money:
     bet = int(input("It looks like you lack the funds. Please enter a smaller bet: "))
-  pick = input("Enter 1 for \"Heads\" or 2 for \"Tails\": ")
+  # get input for guess
+  guess = input("Enter 1 for \"Heads\" or 2 for \"Tails\": ")
   choices = {"1": "Heads", "2": "Tails"}
-  # checl if the pick is valid
-  if pick not in list(choices):
-    pick = ("You need to enter 1 or 2. ")
+  # check if the guess is valid
+  if guess not in list(choices):
+    guess = input("You need to enter 1 or 2. ")
+  # display choice
+  print("You have placed a £{0} bet on {1}.".format(bet, choices[guess]))
+  print("Flipping...")
   # flip the coin
-  print("You have placed a £{0} bet on {1}.".format(bet, choices[pick]))
-  print("Flipping...") # delay for suspence
   flip_result = random.choice(list(choices.values()))
   print(flip_result)
-  # display results and increment wallet
-  if flip_result == choices[pick]:
+  # see if we have won and increment wallet
+  if flip_result == choices[guess]:
     money += bet
     return print("Woohoo! You have just won £{0}! Your wallet now has £{1}.".format(bet, money))
   else:
@@ -34,15 +37,59 @@ def coinflip():
     return print("No luck this time... :( You have  lost -£{0}! Your wallet now has £{1}.".format(bet, money))
 
 
+# Cho_Han
+
+def cho_han():
+  # will increment money in global wallet
+  global money
+  # get input for bet (repeats)
+  print("Welcome to Cho_Han!")
+  print("You have £{0} in your wallet.".format(money))
+  bet = int(input("How much money would you like to bet? Please enter a whole number: "))
+  if bet > money:
+    bet = int(input("It looks like you lack the funds. Please enter a smaller bet: "))
+  # get input for guess
+  guess = input("Enter 1 for \"Odd\" or 2 for \"Even\": ")
+  choices = {"1": "Odd", "2": "Even"}
+  # check if the guess is valid
+  if guess not in list(choices):
+    guess = input("You need to enter 1 or 2. ")
+  # display choice
+  print("You have placed a £{0} bet on {1}.".format(bet, choices[guess]))
+  print("Rolling the first dice...")
+  # roll the dice
+  dice1 = random.randint(1, 6)
+  print(dice1)
+  print("Rolling the second dice...")
+  dice2 = random.randint(1, 6)
+  print(dice2)
+  roll_result = dice1 + dice2
+  print("The roll sum is: {0}".format(roll_result))
+  # see if we have won and increment wallet
+  if roll_result // 2 ==0: # roll sum is Even
+    print("That was Even-tful!")
+    if guess == "2": # correct guess
+      money += bet
+      return print("Woohoo! You have just won £{0}! Your wallet now has £{1}.".format(bet, money))
+    else: # wrong guess
+      money -= bet
+      return print("No luck this time... :( You have  lost -£{0}! Your wallet now has £{1}.".format(bet, money))
+  else: # roll sum is Odd
+    print("That's Odd...")
+    if guess == "1": # correct guess
+      money += bet
+      return print("Woohoo! You have just won £{0}! Your wallet now has £{1}.".format(bet, money))
+    else: # wrong guess
+      money -= bet
+      return print("No luck this time... :( You have  lost -£{0}! Your wallet now has £{1}.".format(bet, money))
+
 
 
 
 # TESTING AREA #
 
-coinflip()
-
+# coinflip()
 print(money)
 
-coinflip()
-
+cho_han()
 print(money)
